ed5aa3d79f283244ae2384d8ac6690f52e5dcd6a
Oleg <oarch@mail.ru>
2015-03-05 20:17:16 +0300

    Модель новости

---
+++ models/NewsModel.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Олег
+ * Date: 03.03.15
+ * Time: 16:48
+ */
+
+class NewsModel
+      extends AbstractModel
+{
+     protected static $table = 'news';
+}
