56efd182ae7aa2607fb958c72ea645cf7a9b9bc6
Oleg <oarch@mail.ru>
2015-03-05 20:18:21 +0300

    Автозагрузчик

---
+++ settings/autoload.php
@@ -0,0 +1,16 @@
+<?php
+
+function __autoload($class){
+
+    $levels = ['/','/../','/../../','/../../../'];
+    $paths = ['/','/controllers/','/classes/','/models/','/views/','/views/news/','/functions/','/settings/'];
+
+    foreach ($levels as $level):
+        foreach ($paths as $path):
+            if (file_exists(__DIR__ . $level . $path . $class .'.php')):
+                require_once __DIR__ . $level . $path . $class.'.php';
+                break 2;
+            endif;
+        endforeach;
+    endforeach;
+}
