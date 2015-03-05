a66b7ab7a418a2baf098cc7c889d67d97e16a529
Oleg <oarch@mail.ru>
2015-03-05 20:27:02 +0300

    тест

--- l5.php
+++ l5.php
@@ -1,3 +1,29 @@
 <?php
 
+require_once __DIR__.'/settings/autoload.php';
+
 echo 'Урок 5 <br>';
+
+$news = new Admin;
+
+$iterator = new MyIterator;
+
+$iterator = $news->getAll();
+
+//Ar::dump($iterator);
+var_dump($iterator);
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$keys = array_keys($iterator);
+var_dump($keys);
+
+echo 'array_keys = '. $keys .'<br>';
+if (!empty($iterator)) {
+    echo '$iterator не пуст';
+    echo $iterator[0]->title;
+}else{
+    echo '$iterator пуст';
+}
